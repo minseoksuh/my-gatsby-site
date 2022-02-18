6a118e102e6e37e46b24ac6aaf23c5145581724f
USER <user@AL01277146.local>
2022-02-18 12:51:21 +0900

    feat: useStaticQuery to add blog post

---
+++ src/pages/blog.js
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import { graphql } from 'gatsby'
+
+const BlogPage = ({data}) => {
+    
+    
+  return (
+    <Layout pageTitle="My Blog Posts">
+        <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+ query {
+  allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+    nodes {
+      name
+    }
+  }
+}
+`
+
+export default BlogPage
